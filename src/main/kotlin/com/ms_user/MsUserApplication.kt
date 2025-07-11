551e49b9f23c3afc88fd793c35fc7c4ef00fc9ea
Bruna Suzuki <brunasuzuki04@gmail.com>
2025-07-11 12:07:26 -0300

    Initial config

---
+++ src/main/kotlin/com/ms_user/MsUserApplication.kt
@@ -0,0 +1,11 @@
+package com.ms_user
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsUserApplication
+
+fun main(args: Array<String>) {
+	runApplication<MsUserApplication>(*args)
+}
